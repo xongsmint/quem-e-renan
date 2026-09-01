23d75444c19867f107b17f03db584edc75cd0866
João Zacchello <xongs@192.168.68.108>
2026-09-01 19:44:03 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  base: "/quem-e-renan/"
 })
